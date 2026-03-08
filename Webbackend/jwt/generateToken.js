a9dffba90849447ab3ed4b27081b74d1906d22cd
Harsh Suryavanshi <Harsh-Dev1602@users.noreply.github.com>
2026-03-08 16:37:06 +0530

    placiddigital -[#08032026]

--- Webbackend/jwt/generateToken.js
+++ Webbackend/jwt/generateToken.js
@@ -1,13 +1,13 @@
 import jwt from "jsonwebtoken";
 
 const createTokenAndSaveCookie = (adminId, res) => {
-  const token = jwt.sign({ adminId }, process.env.ADMIN_JWT_KEY,{
+  const token = jwt.sign({ adminId }, process.env.ADMIN_JWT_KEY, {
     expiresIn: "1d",
   });
   res.cookie("Admin_Key", token, {
-    httpOnly: false, // xss
-    secure: false,
-    sameSite: "Lax", // csrf
+    httpOnly: true,        // prevent XSS
+    secure: true,          // required for HTTPS deployment
+    sameSite: "None",      // allow cross-site cookies
   });
 };
 export default createTokenAndSaveCookie;
