c7003d5a12e9e585ad1e14dfbdbc93145c379029
Harsh Suryavanshi <Harsh-Dev1602@users.noreply.github.com>
2026-03-09 15:11:06 +0530

    placiddigital -[#09032026]

--- Webbackend/index.js
+++ Webbackend/index.js
@@ -14,29 +14,32 @@ App.use(express.json());
 App.use(cookieParser());
 App.use(cors());
 App.use(
-  cors({
-    origin: "https://placid-digital.vercel.app",
-  })
+     cors({
+          origin: "https://placid-digital.vercel.app",
+          credentials: true,
+          methods: ["GET", "POST", "PUT", "DELETE"],
+          allowedHeaders: ["Content-Type", "Authorization"]
+     })
 );
 
 const PORT = process.env.PORT || 3001;
 const DB_URL = process.env.MONGO_DB_URL
-try{
+try {
      mongoose.connect(DB_URL);
      console.log("Mongo DB connected..");
      await seedAdmin();
-}catch (error){
+} catch (error) {
      console.log(error);
 }
 
-App.get('/',(req,res)=>{
+App.get('/', (req, res) => {
      res.send('hello');
 })
 
-App.use("/sfs-app/course",courseRouter);
-App.use("/sfs-app/upload",uploadRouter);
-App.use("/sfs-app/admin",adminRouter)
+App.use("/sfs-app/course", courseRouter);
+App.use("/sfs-app/upload", uploadRouter);
+App.use("/sfs-app/admin", adminRouter)
 
-App.listen(PORT,()=>{
-    console.log(`WebBackend Run on this port http://localhost:${PORT}`)
+App.listen(PORT, () => {
+     console.log(`WebBackend Run on this port http://localhost:${PORT}`)
 })
